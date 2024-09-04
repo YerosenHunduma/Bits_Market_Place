0d6f93d83eb3272fb22a7e1c0f0147d8eb24cb4a
YerosenHunduma <yerosenhunduma@gmail.com>
2024-09-05 01:24:35 +0300

    create bid and product schema

--- models/bid.model.js
+++ models/bid.model.js
@@ -1,5 +1,16 @@
 import { Schema, model } from 'mongoose';
 
-const bidSchema = new Schema({});
+const bidSchema = new Schema(
+    {
+        product: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
+        bidder: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+        amount: { type: Number, required: true },
+        status: { type: String, enum: ['pending', 'accepted', 'rejected'], default: 'pending' }
+    },
+    {
+        timestamps: true,
+        versionKey: false
+    }
+);
 
 export default model('Bid', bidSchema);
--- models/product.model.js
+++ models/product.model.js
@@ -1,5 +1,29 @@
 import { Schema, model } from 'mongoose';
 
-const productSchema = new Schema({});
+const productSchema = new Schema(
+    {
+        name: { type: String, required: true },
+        description: { type: String, required: true },
+        seller: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+        images: [
+            {
+                public_id: {
+                    type: String,
+                    required: true
+                },
+                secure_url: {
+                    type: String,
+                    required: true
+                }
+            }
+        ],
+        price: { type: Number, required: true },
+        isApproved: { type: Boolean, default: false },
+        status: { type: String, enum: ['available', 'sold', 'lent', 'pending'], default: 'pending' },
+        bids: [{ type: Schema.Types.ObjectId, ref: 'Bid' }],
+        category: { type: Schema.Types.ObjectId, ref: 'Category', required: true }
+    },
+    { timestamp: true, versionKey: true }
+);
 
 export default model('Product', productSchema);
