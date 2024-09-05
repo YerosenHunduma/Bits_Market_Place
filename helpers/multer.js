2d1124ea114293e9cac88d11aa7d380d29a661f9
YerosenHunduma <yerosenhunduma@gmail.com>
2024-09-05 12:02:32 +0300

    create a multer helper file to upload image to server

---
+++ helpers/multer.js
@@ -0,0 +1,38 @@
+import fs from 'fs';
+import multer from 'multer';
+import path from 'path';
+import { errorHandler } from '../utils/errorHandler';
+
+if (!fs.existsSync('./uploads')) {
+    fs.mkdirSync('./uploads');
+}
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './uploads');
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
+    }
+});
+
+const checkFiletype = (file, cb) => {
+    const filetypes = /jpeg|jpg|png/;
+
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = filetypes.test(file.mimetype);
+
+    if (extname && mimetype) {
+        cb(null, true);
+    } else {
+        cb(new errorHandler('Invalid file type, You can only upload jpeg, jpg and png', 400), false);
+    }
+};
+
+export const uploadImageFromLocalToServer = multer({
+    storage,
+    // limits: { filesize: 1024 * 1024 },
+    fileFilter: (req, file, cb) => {
+        checkFiletype(file, cb);
+    }
+});
