1a911729597e6461d516a7ce8877a550fab9d58f
YerosenHunduma <yerosenhunduma@gmail.com>
2024-09-15 21:05:12 +0300

    update payment model

--- models/payment.model.js
+++ models/payment.model.js
@@ -11,10 +11,17 @@ const paymentSchema = new Schema(
         tx_ref: { type: String },
         productName: { type: String },
         status: { type: String, enum: ['pending', 'failed', 'success'], default: 'pending' },
-        customerId: { type: String }
+        customerId: { type: String },
+        currency: { type: String },
+        chapa_transactio_charge: { type: String },
+        bits_transaction_charge: { type: String },
+        balance: { type: String },
+        payment_method: String,
+        created_at: String,
+        updated_at: String,
+        reference: String
     },
     {
-        timestamps: true,
         versionKey: false
     }
 );
