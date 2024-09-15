4d4a93a32e66ec2b53201d789061a56bcc33c5bf
YerosenHunduma <yerosenhunduma@gmail.com>
2024-09-15 17:22:43 +0300

    create an endpoint for bid actions

--- models/bid.model.js
+++ models/bid.model.js
@@ -5,6 +5,8 @@ const bidSchema = new Schema(
         productId: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
         bidderId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
         amount: { type: Number, required: true },
+        productName: { type: String, required: true },
+        sellerId: { type: String, required: true },
         message: { type: String },
         status: { type: String, enum: ['pending', 'accepted', 'rejected'], default: 'pending' }
     },
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ import globalErrorHandler from '../middlewares/globalErrorHandler.js';
 import categoryRoutes from './category.routes.js';
 import userRoutes from './user.routes.js';
 import bidRoutes from './bid.routes.js';
+import paymentRoutes from './payment.routes.js';
 import { isAuthenticated } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
@@ -14,6 +15,7 @@ router.use('/product', productRoutes);
 router.use('/category', categoryRoutes);
 router.use('/user', userRoutes);
 router.use('/bid', isAuthenticated, bidRoutes);
+router.use('/payment', isAuthenticated, paymentRoutes);
 router.use(globalErrorHandler);
 
 export default router;
